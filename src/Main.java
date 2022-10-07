df934f859e3bca543caddafdf6c2e7e912f843ab
Elena <elenasenina0111@gmail.com>
2022-10-07 17:17:14 -0500

    my first intel commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] args)
+
+
+    {
+        int a = 10;
+        int b = 2;
+        char c = 'Z' / 2;
+
+        long number;
+        number = 232550000000L;
+
+        System.out.println( number );
+
+        System.out.println( c );
+        System.out.println( c+0 );
+
+//        c = 'B' + 1;
+        c = 'D' + 1;
+
+    System.out.println( c+0 );
+    c = 65;
+
+    System.out.println( c );
+
+  float f = 2.346511445F;
+  double num = 2.345678;
+
+  boolean isTrue;
+  isTrue = 1 == 1;
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
